2eae2976d8b097183a448f1e1bdbdcba8f8027bf
Rahguzar <rahguzar@zohomail.eu>
2024-11-10 16:40:39 +0500

    Update ELPA version

--- consult-hoogle.el
+++ consult-hoogle.el
@@ -6,7 +6,7 @@
 ;; Maintainer: rahguzar <rahguzar@zohomail.eu>
 ;; Created: April 10, 2022
 ;; License: GPL-3.0-or-later
-;; Version: 0.2.2
+;; Version: 0.3.0
 ;; Keywords: docs languages
 ;; Homepage: https://codeberg.org/rahguzar/consult-hoogle
 ;; Package-Requires: ((emacs "28.1") (consult "0.18"))
